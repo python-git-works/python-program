42e065b67d412d1857ff110e87dbd77887418f41
malay_git <malaydixit0412@gmail.com>
2025-05-05 00:37:06 +0530

    Both assignment added

---
+++ Assignment/Assignment_1.py
@@ -0,0 +1,23 @@
+# Task 1: Perform Basic Mathematical Operations
+
+a=input('First Input from User:-')
+b=input('Second Input from User:-')
+
+a=int(a)
+b=int(b)
+
+#Addition
+c= a + b
+print('Addition:- ',c)
+
+#Subtraction
+d= a - b
+print('Subtraction:- ',d)
+
+#Multiplication
+e= a * b
+print('Multiplication:- ',e)
+
+#Division
+f= a / b
+print('Division:- ',f)
---
+++ Assignment/Assignment_2.py
@@ -0,0 +1,9 @@
+#Task 2: Create a Personalized Greeting
+import concat
+
+first=input('Enter your first Name:- ')
+last=input('Enter your last Name:- ')
+
+full_name = first + " " + last
+
+print("Hello, ", full_name,"!", "Welcome to the python program")
